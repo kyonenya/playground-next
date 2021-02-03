5adc2988e5b2e13a17e994c4b16896ac1449a0e0
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-04 07:34:52 +0900

    feat: 動的ルート PCで動作検証

---
+++ pages/entries/[id].js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import fetch from 'node-fetch';
+
+export default function Entry() {
+  return (
+    <h1>entry</h1>
+  );
+}
+
+export function getStaticPaths() {
+//  const limit = 100;
+//  const url = `https://manuscripts.herokuapp.com/api/entries?limit=${limit}`;
+//  const entries = await fetch(url).then(response => response.json());
+//  const paths = entries.map(entry => ({
+//    params: { id: entry.uuid }
+//  }));
+//  return {
+//    paths,
+//    fallback: false,
+//  };
+  return {
+    paths: [
+      { params: { id: '1' } },
+      { params: { id: '2' } },
+    ],
+    fallback: false,
+  }
+}
+
+// 'C2E65E4910424922A29B160BE869B57B'
